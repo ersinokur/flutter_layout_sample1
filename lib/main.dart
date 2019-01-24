c22b9116f66fd1d1bd60a9c1beb060497c631ed3
Ersin Okur <ersin.okur@gmail.com>
2019-01-24 17:53:18 +0300

    secon commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+  
     return MaterialApp(
       title: 'Flutter Layout Demo',
       theme: ThemeData(
@@ -25,46 +26,95 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  
   Widget build(BuildContext context) {
+
     return Scaffold(
         appBar: AppBar(
-          title: Text('Flatter layout sample 1'),
+          title: Text('Flatter layout samplwe'),
         ),
         body: Column(
           children: <Widget>[
-            titleSection,
+            titleSection(),
+           buttonSection(context),
           ],
-        ));
+        ));        
   }
 
-  Widget titleSection = Container(
-    padding: const EdgeInsets.all(32.0),
-    child: Row(
-      children: [
-        // steretch column to use all remaining free space in the row.
-        Expanded(
-          // 1
-          child: Column(
-            //postions the columns at the start of the row.
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+  Widget titleSection() {
+  
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(22.0),
+          child: Row(
             children: [
-              // 2
-              Container(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: Text(
-                    'Yedigöller kamp alanı',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  )),
-              Text(
-                'Bolu Turkey',
-                style: TextStyle(color: Colors.grey[500]),
+              // steretch column to use all remaining free space in the row.
+              Expanded(
+                // 1
+                child: Column(
+                  //postions the columns at the start of the row.
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // 2
+                    Container(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Text(
+                          'Yedigöller kamp alanı',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )),
+                    Text(
+                      'Bolu Turkey',
+                      style: TextStyle(color: Colors.grey[500]),
+                    ),
+                  ],
+                ),
               ),
+              Icon(Icons.star, color: Colors.red[500]),
+              Text('44')
             ],
           ),
         ),
-        Icon(Icons.star,color:Colors.red[500]),
-        Text('44')
       ],
-    ),
+    );
+
+    
+  }
+
+ 
+Widget buttonSection (BuildContext context) {
+  Color color = Theme.of(context).primaryColor;
+  return Container(
+child: Row(
+  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  children: <Widget>[
+   _buildButtonColumn(color, Icons.call, "ara"),
+   _buildButtonColumn(color, Icons.near_me, "rota"),
+   _buildButtonColumn(color, Icons.share, "paylaş"),
+
+  ],)
+
   );
 }
+
+
+}
+
+
+Column _buildButtonColumn(Color color, IconData icon, String label) {
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Icon(icon, color: color),
+      Container(
+          margin: const EdgeInsets.only(top: 8.0),
+          child: Text(
+            label,
+            style: TextStyle(
+                fontSize: 12.0, fontWeight: FontWeight.w400, color: color),
+          )),
+    ],
+  );
+}
+
